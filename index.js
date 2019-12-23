775183f6182bdbb5784eb0ef46c78bc3b6c68bc3
Cyril Caillat <cyril.caillat@free.fr>
2019-12-23 11:17:55 +0100

    Gestion d'un mode ajout modification inline de la liste

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const session = require('telegraf/session');
 const TelegrafI18n = require('telegraf-i18n');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
-/**
- * mode 'inline', affichage de la liste dans les messages
- * mode 'keyboard', affichage de la liste sous forme de keyboard
- */
-const mode = "inline";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const i18n = new TelegrafI18n({
@@ -23,6 +18,10 @@ const i18n = new TelegrafI18n({
     allowMissing: true,
     directory: path.resolve(__dirname, 'locales')
 });
+
+const cmdList = 'list';
+const cmdAdd = 'add';
+
 bot.use(session());
 bot.use(i18n.middleware())
 bot.use((ctx, next) => {
@@ -41,6 +40,7 @@ bot.command('delall', (ctx) => {
     Todo.deleteMany({ 'chatId': ctx.chat.id }, function (err) {
         ctx.reply(ctx.from.first_name + ' ' + ctx.from.last_name + ' ' + ctx.i18n.t('deleteAllReturn'));
     });
+    setUserModeAdd(ctx, false);
 });
 /**
  * jeu de test
@@ -50,6 +50,7 @@ bot.command('test', (ctx) => {
     var text = "chocolat,farine,sucre,oeufs,boeuf,poulet,saumon,levure,salade,coca,jus de fruits,eau pétillante,desserts,glace vanille,sorbets";
     addTodo(ctx, text);
     findAllTodosByUser(ctx, 'text', 'Init list ' + text);
+    setUserModeAdd(ctx, false);
 });
 /**
  * Commande ajout
@@ -61,8 +62,10 @@ bot.command('add', (ctx) => {
         text = ctx.message.text.substring(5);
         if (text.length > 0) {
             addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text) });
-        } else
-            ctx.reply(ctx.i18n.t('addWhat1'), { reply_to_message_id: ctx.message.message_id, force_reply: true, selective: true });
+        } else {
+            ctx.reply(ctx.i18n.t('addWhat'), { reply_to_message_id: ctx.message.message_id, force_reply: true, selective: true });
+            setUserModeAdd(ctx, true);
+        }
     } else {
         findAllTodosByUser(ctx);
     }
@@ -72,10 +75,11 @@ bot.command('add', (ctx) => {
  */
 bot.command('list', (ctx) => {
     console.log("command list");
-    findAllTodosByUser(ctx, ctx.message.text.substring(6), ctx.i18n.t('list'));
+    setUserModeAdd(ctx, false);
+    findAllTodosByUser(ctx, ctx.message.text.substring(6), cmdList);
 });
 /**
- * En mode 'inline', la suppression se fait par callback sur l'id
+ * La suppression se fait par callback sur l'id
  */
 bot.on('callback_query', ctx => {
     // get info from callback_query object
@@ -90,121 +94,84 @@ bot.on('callback_query', ctx => {
                 if (error) {
                     ctx.reply(error);
                     console.log(error);
+                } else {
+                    //ctx.reply(ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
+                    findAllTodosByUser(ctx, "text", ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
                 }
-                findAllTodosByUser(ctx, "text", ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
+
             })
         } else {
             findAllTodosByUser(ctx);
         }
     });
+    setUserModeAdd(ctx, false);
 });
-/**
- * Mode 'keyboard', la suppression se fait par nom
- * Mode 'inline', teste '🔍 Liste'
- */
 bot.on('text', (ctx) => {
     console.log("text", ctx.update.message.text);
-    const button_list = ctx.i18n.t('button_list');
     var text = ctx.update.message.text;
-    if (ctx.update.message.text == button_list) {
-        findAllTodosByUser(ctx, 'text', ctx.i18n.t('list'));
-    } else if (mode == 'inline' && typeof ctx.update.message.message_id != "undefined") {
-        if (text.startsWith('/')) {
-            if (text.indexOf(' ') > 0)
-                text = text.substring(text.indexOf(' ') + 1);
-            else text = '';
-        }
-        if (text.length > 0)
-            addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text) });
-    } else if (mode == 'keyboard') {
-        findTodoByText(ctx, text, function (err, data) {
-            if (data != null) {
-                console.log("callback id", data._id);
-                Todo.deleteOne({
-                    "_id": data._id
-                }, function (error, todo) {
-                    if (error) {
-                        ctx.reply(error);
-                        console.log(error);
-                    }
-                    //console.log(error, data);
-                    findAllTodosByUser(ctx, "text", ctx.update.message.from.first_name + ' ' + ctx.update.message.from.last_name + " a supprimé " + data.text);
-                })
-            } else {
-                findAllTodosByUser(ctx, 'text', button_list);
-            }
-        });
+    if (text.startsWith('/')) {
+        if (text.indexOf(' ') > 0)
+            text = text.substring(text.indexOf(' ') + 1);
+        else text = '';
     }
+    if (text.length > 0 && isUserModeAdd(ctx))
+        addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text) });
 });
 bot.catch((err, ctx) => {
     console.log('Ooops, ecountered an error for ${ctx.updateType}', err)
 });
 bot.launch();
 /**
- * Affiche de la liste en mode 'inline' ou 'keyboard'.
- * En mode 'inline', on affiche un keyboard, avec la commande Liste
+ * Affiche de la liste
+ *
  * @param {*} ctx 
  * @param {*} sort, trié par text ou date 
- * @param {*} title, titre, en mode inline, si title='liste', alors on affiche la liste
+ * @param {*} title, titre
  */
 function findAllTodosByUser(ctx, sort, title) {
     console.log("findAllTodosByUser", sort, title);
-    const button_list = ctx.i18n.t('button_list');
-    if (false && mode == "inline" && (typeof title == "undefined" || title.toLowerCase() != ctx.i18n.t('list').toLowerCase())) {
-        const keyboard = Markup.keyboard([Markup.callbackButton(button_list, '/list')])
-            .resize()
-            .oneTime()
-            .extra();
-        ctx.reply(typeof title == "undefined" ? ctx.i18n.t('list') : title, keyboard);
-    } else {
-        if (sort != "date") sort = "text";
-        Todo.find()
-            .where('chatId')
-            .equals(ctx.chat.id)
-            .sort(sort)
-            .exec(function (err, todos) {
-                if (err) {
-                    console.log(err);
+    if (sort != "date") sort = "text";
+    if (title !== cmdList) ctx.reply(title);
+    Todo.find()
+        .where('chatId')
+        .equals(ctx.chat.id)
+        .sort(sort)
+        .exec(function (err, todos) {
+            if (err) {
+                console.log(err);
+            }
+            var arrayReply0 = new Array();
+            var arrayReply1 = new Array();
+            var arrayReply2 = new Array();
+            var cpt = 0;
+            for (var todo in todos) {
+                arrayReply0.push(todos[todo].text);
+                arrayReply2.push(Markup.callbackButton(todos[todo].text, todos[todo].id));
+                cpt++;
+                if (cpt != 0 && cpt % 4 == 0) {
+                    arrayReply1.push(arrayReply2);
+                    arrayReply2 = new Array();
                 }
-                var arrayReply0 = new Array();
-                var arrayReply1 = new Array();
-                var arrayReply2 = new Array();
-                var cpt = 0;
-                for (var todo in todos) {
-                    arrayReply0.push(todos[todo].text);
-                    arrayReply2.push(Markup.callbackButton(todos[todo].text, todos[todo].id));
-                    cpt++;
-                    if (cpt != 0 && cpt % 4 == 0) {
-                        arrayReply1.push(arrayReply2);
-                        arrayReply2 = new Array();
-                    }
 
-                }
-                //ctx.reply(arrayReply0.join(','));
-                arrayReply1.push(arrayReply2);
-                console.log(arrayReply0.join(', ').length);
-                if (mode == 'inline') {
-                    if (ctx.updateType == "callback_query" && typeof title != "undefined" && typeof title != ctx.i18n.t('list'))
-                        ctx.answerCbQuery(title);
-                    ctx.reply(arrayReply0.join(', ').length == 0 ? ctx.i18n.t('empty') : arrayReply0.join(', '), Markup.inlineKeyboard(arrayReply1)
-                        //.resize()
-                        .oneTime()
-                        .extra());
-                    //ctx.reply( ctx.i18n.t('list'),
-                    //    Markup.keyboard([Markup.callbackButton(button_list, '/list')])
-                    //        .resize()
-                    //        .oneTime()
-                    //        .extra()
-                    //);
+            }
+            //ctx.reply(arrayReply0.join(','));
+            arrayReply1.push(arrayReply2);
+            const reply = arrayReply0.join(', ').length == 0 ? ctx.i18n.t('empty') : arrayReply0.join(', ');
+            const markup = Markup.inlineKeyboard(arrayReply1).oneTime().extra();
+            //console.log(arrayReply0.join(', ').length);
+            if (reply.length > 0) {
+                if (title !== cmdList && typeof ctx.session.listMessageId !== 'undefined') {
+                    ctx.telegram.editMessageText(ctx.chat.id, ctx.session.listMessageId, ctx.session.listInlineMessageId, reply, markup).then(function (replyCtx) { });
                 } else {
-                    ctx.reply(typeof title == "undefined" ? ctx.i18n.t('list') : title,
-                        Markup.keyboard(arrayReply1)
-                            .resize()
-                            .extra()
-                    );
+                    ctx.reply(reply, markup)
+                        .then(function (replyCtx) {
+                            console.log("replyCtx", replyCtx);
+                            ctx.session.listMessageId = replyCtx.message_id;
+                            ctx.session.listInlineMessageId = replyCtx.inline_message_id
+                        });
                 }
-            });
-    }
+            }
+        });
 }
 /**
  * Recherche par id
@@ -216,7 +183,7 @@ function findTodoById(ctx, id, callback) {
     Todo.findOne({ $and: [{ '_id': id }, { 'chatId': ctx.chat.id }] })
         .exec(function (err, todo) {
             if (err) console.log("findTodosById", err);
-            console.log("findTodosById", todo);
+            //console.log("findTodosById", todo);
             callback(err, todo);
         });
 }
@@ -252,4 +219,40 @@ function addTodo(ctx, text, callback) {
             if (callback) callback(err, data);
         });
     } else if (callback) callback();
+}
+/**
+ * 
+ * @param {*} ctx 
+ */
+function isUserModeAdd(ctx) {
+    return getUserSession(ctx).modeAdd;
+}
+function setUserModeAdd(ctx, value) {
+    var user = getUserSession(ctx);
+    user.modeAdd = value;
+}
+/**
+ * 
+ * @param {*} ctx 
+ */
+function getUserSession(ctx) {
+    if (typeof ctx.session.users === 'undefined') ctx.session.users = {};
+    var user = {};
+    var id = ctx.from.id;
+    if (ctx.session.users.hasOwnProperty(id)) {
+        user = ctx.session.users[id];
+    } else {
+        user.modeAdd = false;
+        ctx.session.users[id] = user;
+    }
+    return user;
+}
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} datas 
+ */
+function setUserSession(ctx, datas) {
+    if (typeof ctx.session.users === 'undefined') ctx.session.users = new Array();
+    ctx.session.users[ctx.from.id] = datas;
 }
