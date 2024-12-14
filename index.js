b2fd77f23a687d46f5ecf2a5ff8da10479963b50
Cyril <cyril@caillat.ovh>
2024-12-14 20:04:21 +0100

    Mongodb 8
    Passage à mongodb8

--- index.js
+++ index.js
@@ -62,22 +62,26 @@ bot.command('test', (ctx) => {
  * Commande ajout
  */
 bot.command('add', (ctx) => {
-    console.log("command add", ctx.message.text);
-    var text = ctx.message.text;
-    if (text.startsWith('/add@'))
-        text = '/add';
-    if (text.startsWith('/add')) {
-        text = text.substring(5);
-        if (text.length > 0) {
-            addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text) });
+    try {
+        console.log("command add", ctx.message.text);
+        var text = ctx.message.text;
+        if (text.startsWith('/add@'))
+            text = '/add';
+        if (text.startsWith('/add')) {
+            text = text.substring(5);
+            if (text.length > 0) {
+                addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text) });
+            } else {
+                //ctx.reply(ctx.i18n.t('addWhat'), Markup.forceReply().extra({selective:true}));
+                ctx.reply(ctx.i18n.t('addWhat'), Markup.forceReply().extra({ selective: true, disable_notification: true }));
+                //ctx.reply(ctx.i18n.t('addWhat'), {selective:true,disable_notification:true,reply_to_message_id:ctx.message.message_id});
+                setUserModeAdd(ctx, true);
+            }
         } else {
-            //ctx.reply(ctx.i18n.t('addWhat'), Markup.forceReply().extra({selective:true}));
-            ctx.reply(ctx.i18n.t('addWhat'), Markup.forceReply().extra({ selective: true, disable_notification: true }));
-            //ctx.reply(ctx.i18n.t('addWhat'), {selective:true,disable_notification:true,reply_to_message_id:ctx.message.message_id});
-            setUserModeAdd(ctx, true);
+            findAllTodosByUser(ctx);
         }
-    } else {
-        findAllTodosByUser(ctx);
+    } catch (e) {
+        console.log(e);
     }
 });
 /**
@@ -92,45 +96,53 @@ bot.command('list', (ctx) => {
  * La suppression se fait par callback sur l'id
  */
 bot.on('callback_query', ctx => {
-    // get info from callback_query object
-    var id = ctx.update.callback_query.data;
-    console.log("callback id", id);
-    findTodoById(ctx, id, function (err, data) {
-        if (data != null) {
-            console.log("callback id", data._id);
-            Todo.deleteOne({
-                "_id": data._id
-            }, function (error, todo) {
-                if (error) {
-                    ctx.reply(error);
-                    console.log(error);
-                } else {
-                    //ctx.reply(ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
-                    findAllTodosByUser(ctx, "text", ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
-                }
-            })
-        } else {
-            findAllTodosByUser(ctx);
-        }
-    });
-    setUserModeAdd(ctx, false);
+    try {
+        // get info from callback_query object
+        var id = ctx.update.callback_query.data;
+        console.log("callback id", id);
+        findTodoById(ctx, id, function (data) {
+            if (data != null) {
+                console.log("callback id", data._id);
+                Todo.deleteOne({
+                    "_id": data._id
+                }).then(function (result) {
+                    if (result.acknowledged) {
+                        //ctx.reply(ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
+                        findAllTodosByUser(ctx, "text", ctx.update.callback_query.from.first_name + ' ' + ctx.update.callback_query.from.last_name + ' ' + ctx.i18n.t('deleted') + ' ' + data.text);
+                    } else {
+                        ctx.reply(result);
+                        console.log(result);
+                    }
+                })
+            } else {
+                findAllTodosByUser(ctx);
+            }
+        });
+        setUserModeAdd(ctx, false);
+    } catch (e) {
+        console.log(e);
+    }
 });
 bot.on('text', (ctx) => {
-    console.log("text", ctx.update.message.text);
-    var text = ctx.update.message.text;
-    if (text.startsWith('/')) {
-        if (text.indexOf(' ') > 0)
-            text = text.substring(text.indexOf(' ') + 1);
-        else text = '';
-    }
-    if (text.length > 0 && isUserModeAdd(ctx))
-        addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text); razUserSession(getUserSession(ctx)); });
-    if (text.toLowerCase() == 'y' || text.toLowerCase() == 'o') {
-        Todo.deleteMany({ 'chatId': ctx.chat.id }, function (err) {
-            ctx.reply(ctx.from.first_name + ' ' + ctx.from.last_name + ' ' + ctx.i18n.t('deleteAllReturn'));
-            razUserSession(getUserSession(ctx));
-        });
+    try {
+        console.log("text", ctx.update.message.text);
+        var text = ctx.update.message.text;
+        if (text.startsWith('/')) {
+            if (text.indexOf(' ') > 0)
+                text = text.substring(text.indexOf(' ') + 1);
+            else text = '';
+        }
+        if (text.length > 0 && isUserModeAdd(ctx))
+            addTodo(ctx, text, function () { findAllTodosByUser(ctx, 'text', ctx.i18n.t('added') + ' ' + text); razUserSession(getUserSession(ctx)); });
+        if (text.toLowerCase() == 'y' || text.toLowerCase() == 'o') {
+            Todo.deleteMany({ 'chatId': ctx.chat.id }, function (err) {
+                ctx.reply(ctx.from.first_name + ' ' + ctx.from.last_name + ' ' + ctx.i18n.t('deleteAllReturn'));
+                razUserSession(getUserSession(ctx));
+            });
 
+        }
+    } catch (e) {
+        console.log(e);
     }
 });
 bot.catch((err, ctx) => {
@@ -144,110 +156,128 @@ bot.launch();
  * @param {*} sort, trié par text ou date 
  * @param {*} title, titre
  */
-function findAllTodosByUser(ctx, sort, title) {
-    console.log("findAllTodosByUser", sort, title);
-    if (sort != "date") sort = "text";
-    if (title !== cmdList && typeof title !== 'undefined' && title.length > 0) ctx.reply(title);
-    Todo.find()
-        .where('chatId')
-        .equals(ctx.chat.id)
-        .sort(sort)
-        .collation({
+async function findAllTodosByUser(ctx, sort, title) {
+    try {
+        console.log("findAllTodosByUser", sort, title);
+        if (sort != "date") sort = "text";
+        if (title !== cmdList && typeof title !== 'undefined' && title.length > 0) ctx.reply(title);
+        const query = { "chatId": ctx.chat.id };
+        const cursor = Todo.find(query).sort(sort).collation({
             locale: "en",
             strength: 2,
             numericOrdering: true,
-        }).exec(function (err, todos) {
-            if (err) {
-                console.log(err);
-            }
-            var arrayReply0 = new Array();
-            var arrayReply1 = new Array();
-            var arrayReply2 = new Array();
-            var cpt = 0;
-            for (var todo in todos) {
-                if (todos[todo].text.trim().length < 1) {
-                    console.log("delete :", todos[todo]._id)
-                    Todo.deleteOne({
-                        "_id": todos[todo]._id
-                    }, function (error, todo) {
-                        console.log(error ? error : "delete ok");
-                    });
-                } else {
-                    arrayReply0.push(todos[todo].text);
-                    arrayReply2.push(Markup.callbackButton(todos[todo].text, todos[todo].id));
-                    cpt++;
-                    if (cpt != 0 && cpt % buttonsByRow == 0) {
-                        arrayReply1.push(arrayReply2);
-                        arrayReply2 = new Array();
-                    }
-                }
-            }
-            //ctx.reply(arrayReply0.join(','));
-            arrayReply1.push(arrayReply2);
-            const reply = arrayReply0.join(', ').length == 0 ? ctx.i18n.t('empty') : arrayReply0.join(', ');
-            const markup = Markup.inlineKeyboard(arrayReply1).oneTime().selective().extra();
-            //console.log(arrayReply0.join(', ').length);
-            if (reply.length > 0) {
-                if (title !== cmdList && typeof ctx.session.listMessageId !== 'undefined') {
-                    ctx.telegram.editMessageText(ctx.chat.id, ctx.session.listMessageId, ctx.session.listInlineMessageId, reply, markup).then(function (replyCtx) { });
-                } else {
-                    ctx.reply(reply, markup)
-                        .then(function (replyCtx) {
-                            console.log("replyCtx", replyCtx);
-                            ctx.session.listMessageId = replyCtx.message_id;
-                            ctx.session.listInlineMessageId = replyCtx.inline_message_id
-                        });
-                }
-            }
         });
+        // Print a message if no documents were found
+        if ((await Todo.countDocuments(query)) === 0) {
+            console.log("No documents found!");
+        }
+        //tableau listes des produits format texte
+        var arrayReply0 = new Array();
+        //tableau boutons par ligne
+        var arrayReply1 = new Array();
+        //tableau bouton en colonne
+        var arrayReply2 = new Array();
+        var cpt = 0;
+        for await (const todo of cursor) {
+            console.dir(todo);
+            if (todo.text.trim().length < 1) {
+                console.log("delete :", todo._id)
+                Todo.deleteOne({
+                    "_id": todo._id
+                }, function (error, todo) {
+                    console.log(error ? error : "delete ok");
+                });
+            } else {
+                arrayReply0.push(todo.text);
+                arrayReply2.push(Markup.callbackButton(todo.text, todo.id));
+                cpt++;
+                if (cpt != 0 && cpt % buttonsByRow == 0) {
+                    arrayReply1.push(arrayReply2);
+                    arrayReply2 = new Array();
+                }
+            }
+        }
+        //ctx.reply(arrayReply0.join(','));
+        arrayReply1.push(arrayReply2);
+        const reply = arrayReply0.join(', ').length == 0 ? ctx.i18n.t('empty') : arrayReply0.join(', ');
+        const markup = Markup.inlineKeyboard(arrayReply1).oneTime().selective().extra();
+        //console.log(arrayReply0.join(', ').length);
+        if (reply.length > 0) {
+            if (title !== cmdList && typeof ctx.session.listMessageId !== 'undefined') {
+                ctx.telegram.editMessageText(ctx.chat.id, ctx.session.listMessageId, ctx.session.listInlineMessageId, reply, markup).then(function (replyCtx) { });
+            } else {
+                ctx.reply(reply, markup)
+                    .then(function (replyCtx) {
+                        console.log("replyCtx", replyCtx);
+                        ctx.session.listMessageId = replyCtx.message_id;
+                        ctx.session.listInlineMessageId = replyCtx.inline_message_id
+                    });
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    } finally {
+    }
 }
 /**
  * Recherche par id
  * @param {*} id 
  * @param {*} callback 
  */
-function findTodoById(ctx, id, callback) {
-    console.log("findTodosById", id);
-    Todo.findOne({ $and: [{ '_id': id }, { 'chatId': ctx.chat.id }] })
-        .exec(function (err, todo) {
-            if (err) console.log("findTodosById", err);
-            //console.log("findTodosById", todo);
-            callback(err, todo);
-        });
+async function findTodoById(ctx, id, callback) {
+    try {
+        console.log("findTodosById", id);
+        const query = {
+            _id: id,
+            chatId: ctx.chat.id
+        }
+        const todo = await Todo.findOne(query);
+        if (callback) callback(todo);
+    } catch (e) {
+        console.log(e);
+    }
 }
 /**
  * Recherche par text
  * @param {*} text 
  * @param {*} callback 
  */
-function findTodoByText(ctx, text, callback) {
-    console.log("findTodosByText", text);
-    Todo.findOne({ $and: [{ 'text': text }, { 'chatId': ctx.chat.id }] })
-        .exec(function (err, todo) {
-            if (err) console.log("findTodosByText", err);
-            console.log("findTodoByText", todo);
-            if (callback) callback(err, todo);
-        });
+async function findTodoByText(ctx, text, callback) {
+    try {
+        console.log("findTodosByText", text);
+        const query = {
+            text: text,
+            chatId: ctx.chat.id
+        }
+        const todo = await Todo.findOne(query);
+        if (callback) callback(todo);
+    } catch (e) {
+        console.log(e);
+    }
 }
 function addTodo(ctx, text, callback) {
-    if (typeof text === 'string') {
-        var textArray = text.split(/\r\n|\r|\n|,|;/);
-        var todos = new Array();
-        for (var i in textArray) {
-            if (textArray[i].trim().length > 0)
-                todos.push({
-                    text: textArray[i].trim(),
-                    done: false,
-                    creator: ctx.from.first_name + ' ' + ctx.from.last_name,
-                    creatorId: ctx.from.id,
-                    chatId: ctx.chat.id
-                });
-        }
-        Todo.insertMany(todos, function (err, data) {
-            if (!err) console.log("added : " + text);
-            if (callback) callback(err, data);
-        });
-    } else if (callback) callback();
+    try {
+        if (typeof text === 'string') {
+            var textArray = text.split(/\r\n|\r|\n|,|;/);
+            var todos = new Array();
+            for (var i in textArray) {
+                if (textArray[i].trim().length > 0)
+                    todos.push({
+                        text: textArray[i].trim(),
+                        done: false,
+                        creator: ctx.from.first_name + ' ' + ctx.from.last_name,
+                        creatorId: ctx.from.id,
+                        chatId: ctx.chat.id
+                    });
+            }
+            Todo.insertMany(todos).then(function (datas) {
+                console.log("added : " + text);
+                if (callback) callback(datas);
+            });
+        } else if (callback) callback();
+    } catch (e) {
+        console.log(e);
+    }
 }
 /**
  * 
@@ -272,17 +302,21 @@ function setUserModeDeleteAll(ctx, value) {
  * @param {*} ctx 
  */
 function getUserSession(ctx) {
-    if (typeof ctx.session.users === 'undefined') ctx.session.users = {};
-    var user = {};
-    var id = getSessionKey(ctx);
-    if (ctx.session.users.hasOwnProperty(id)) {
-        user = ctx.session.users[id];
-        user.sessionExpire = Date.now() + sessionDelay;
-    } else {
-        razUserSession(user);
-        ctx.session.users[id] = user;
+    try {
+        if (typeof ctx.session.users === 'undefined') ctx.session.users = {};
+        var user = {};
+        var id = getSessionKey(ctx);
+        if (ctx.session.users.hasOwnProperty(id)) {
+            user = ctx.session.users[id];
+            user.sessionExpire = Date.now() + sessionDelay;
+        } else {
+            razUserSession(user);
+            ctx.session.users[id] = user;
+        }
+        return user;
+    } catch (e) {
+        console.log(e);
     }
-    return user;
 }
 /**
  * 
@@ -320,13 +354,17 @@ function getSessionKey(ctx) {
  * @param {*} ctx 
  */
 function purgeUserSession(ctx) {
-    if (nextPurge < Date.now()) {
-        if (typeof ctx.session.users === 'undefined') ctx.session.users = {};
-        for (const key in ctx.session.users) {
-            if (ctx.session.users[key].sessionExpire === 'undefined' || ctx.session.users[key].sessionExpire < Date.now()) {
-                delete ctx.session.users[key];
-            }
-        };
-        nextPurge = Date.now() + sessionDelay;
+    try {
+        if (nextPurge < Date.now()) {
+            if (typeof ctx.session.users === 'undefined') ctx.session.users = {};
+            for (const key in ctx.session.users) {
+                if (ctx.session.users[key].sessionExpire === 'undefined' || ctx.session.users[key].sessionExpire < Date.now()) {
+                    delete ctx.session.users[key];
+                }
+            };
+            nextPurge = Date.now() + sessionDelay;
+        }
+    } catch (e) {
+        console.log(e);
     }
 }
