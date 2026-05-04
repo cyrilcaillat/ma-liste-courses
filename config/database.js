58de13b5e003976d281d96191c35164fa1c5bbfe
Cyril <cyril@caillat.ovh>
2026-05-04 21:20:21 +0200

    fix(security): mask DB password in mongoose connection logs

--- config/database.js
+++ config/database.js
@@ -4,15 +4,18 @@ var mongoDatabaseURI = 'mongodb://'
 	+ encodeURIComponent(process.env.DB_PASSWORD) + '@'
 	+ process.env.DB_HOST + ':' + process.env.DB_PORT + '/'
 	+ process.env.DB_DATABASE;
+var safeURI = 'mongodb://' + process.env.DB_USER + ':***@'
+	+ process.env.DB_HOST + ':' + process.env.DB_PORT + '/'
+	+ process.env.DB_DATABASE;
 mongoose.connect(mongoDatabaseURI);
 
 mongoose.connection.on('connected', function() {
-	console.log('Mongoose default connection open to ' + mongoDatabaseURI);
+	console.log('Mongoose default connection open to ' + safeURI);
 });
 
 mongoose.connection.on('error', function(err) {
     console.log('Mongoose default connection error: ' + err);
-    console.log('mongoDatabaseURI :'+mongoDatabaseURI);
+    console.log('mongoDatabaseURI :' + safeURI);
 });
 
 mongoose.connection.on('disconnected', function() {
